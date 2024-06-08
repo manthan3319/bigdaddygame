28438d2a41e0aebbc594771b43d3742585dcb72b
unknown <vaghasiyamanthan8@gmail.com>
2024-06-08 11:35:15 +0530

    add login tab with email login

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react';
-import LoginMenu  from './LoginMenu/page';
+import LoginMenu from './LoginMenu/page';
 import Link from 'next/link';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const Login = () => {
     return (
@@ -8,19 +10,44 @@ const Login = () => {
             <LoginMenu />
             <div className='lg:max-w-[1440px] xl:px-[460px] mx-auto min-h-screen flex items-center justify-center'>
                 <div className='bg-darkgree px-[20px] pt-[15px] w-full max-w-md min-h-[640px] flex flex-col justify-center'>
-                    <div className='text-center mb-[50px]'>
-                        <h1 className='text-yellowlight font-bold text-[19px]'>Log in</h1>
-                    </div>
-                    <div>
-                        <label className="text-[18px] text-white font-jakarta inline-block mb-[4px]">Phone number</label> <br />
-                        <input type='text' className='w-[100%] px-[10px] py-[12px] text-white font-jakarta bg-greey rounded-[5px]' placeholder='Please enter the phone number' /> <br /><br />
-                        <label className="text-[18px] text-white font-jakarta inline-block mb-[4px]">Password</label> <br />
-                        <input type='password' className='w-[100%] px-[10px] py-[12px] text-white font-jakarta bg-greey rounded-[5px]' placeholder='Password' /> <br /><br />
-                        <div className='px-[50px] mt-[20px]'>
-                            <Link href="/" className='text-center font-jakarta font-bold text-[15px] w-[100%] text-white hover:text-black hover:bg-bg inline-block py-[10px] rounded-[50px] border-[2px] border-bg mb-[10px]'>Log in</Link>
-                            <Link href="/Login/Register" className='text-center font-jakarta font-bold text-[15px] w-[100%] text-white hover:text-black hover:bg-bg inline-block py-[10px] rounded-[50px] border-[2px] border-bg mb-[10px]'>Register</Link>
+                    <Tabs>
+                        <div className='text-center mb-[50px]'>
+                            <div className='broder-b-graay border-b-[1px] '>
+                                <TabList className="flex flex-row justify-center gap-[50px]">
+                                    <Tab>
+                                        <h1 className='text-[#888] p-[5px]  text-[18px]'>Log in with phone</h1>
+                                    </Tab>
+                                    <Tab  >
+                                        <h1 className='text-[#888] p-[5px] text-[18px]'>Email / Account</h1>
+                                    </Tab>
+                                </TabList>
+                            </div>
                         </div>
-                    </div>
+                        <TabPanel>
+                            <div>
+                                <label className="text-[18px] text-white font-jakarta inline-block mb-[4px]">Phone number</label> <br />
+                                <input type='text' className='w-[100%] px-[10px] py-[12px] text-white font-jakarta bg-greey rounded-[5px]' placeholder='Please enter the phone number' /> <br /><br />
+                                <label className="text-[18px] text-white font-jakarta inline-block mb-[4px]">Password</label> <br />
+                                <input type='password' className='w-[100%] px-[10px] py-[12px] text-white font-jakarta bg-greey rounded-[5px]' placeholder='Password' /> <br /><br />
+                                <div className='px-[50px] mt-[20px]'>
+                                    <Link href="/" className='text-center font-jakarta font-bold text-[15px] w-[100%] text-white hover:text-black hover:bg-bg inline-block py-[10px] rounded-[50px] border-[2px] border-bg mb-[10px]'>Log in</Link>
+                                    <Link href="/Login/Register" className='text-center font-jakarta font-bold text-[15px] w-[100%] text-white hover:text-black hover:bg-bg inline-block py-[10px] rounded-[50px] border-[2px] border-bg mb-[10px]'>Register</Link>
+                                </div>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <label className="text-[18px] text-white font-jakarta inline-block mb-[4px]">Email / Account Log in</label> <br />
+                                <input type='text' className='w-[100%] px-[10px] py-[12px] text-white font-jakarta bg-greey rounded-[5px]' placeholder='Please input your email' /> <br /><br />
+                                <label className="text-[18px] text-white font-jakarta inline-block mb-[4px]">Password</label> <br />
+                                <input type='password' className='w-[100%] px-[10px] py-[12px] text-white font-jakarta bg-greey rounded-[5px]' placeholder='Password' /> <br /><br />
+                                <div className='px-[50px] mt-[20px]'>
+                                    <Link href="/" className='text-center font-jakarta font-bold text-[15px] w-[100%] text-white hover:text-black hover:bg-bg inline-block py-[10px] rounded-[50px] border-[2px] border-bg mb-[10px]'>Log in</Link>
+                                    <Link href="/Login/Register" className='text-center font-jakarta font-bold text-[15px] w-[100%] text-white hover:text-black hover:bg-bg inline-block py-[10px] rounded-[50px] border-[2px] border-bg mb-[10px]'>Register</Link>
+                                </div>
+                            </div>
+                        </TabPanel>
+                    </Tabs>
                 </div>
             </div>
         </>
